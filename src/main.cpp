fbf2fa56aa2f5441a78a0f305b47fd75961e5861
mohammed-islam <mi.benyahkem@esi-sba.dz>
2024-05-16 17:59:23 +0100

    bug fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,12 @@
 #define A9G_baudRate 115200
 #define baudRate 115200
 #define A9G_rstPin D4
-
-SoftwareSerial sfw(D1, D2);
+#define buzzer_pin D2
+#define A9G_PON 16  // ESP12 GPIO16 A9/A9G POWON
+#define A9G_POFF 15 // ESP12 GPIO15 A9/A9G POWOFF
+#define A9G_WAKE 13 // ESP12 GPIO13 A9/A9G WAKE
+#define A9G_LOWP 2  // ESP12 GPIO2 A9/A9G ENTER LOW POWER MODUL
+SoftwareSerial sfw(14, 12);
 // define the MQTT settings
 const String broker = "test.mosquitto.org";
 String subTopic = "zamalSub";
@@ -24,13 +28,20 @@ void setup()
   Serial.println();
   Serial.println("I am starting\n\r");
   // setup the pins
-  pinMode(A9G_rstPin, OUTPUT);
+  // pinMode(A9G_rstPin, OUTPUT);
 
   Serial.println("A9G is resetting\n\r");
   // resetting the A9G
+  pinMode(D2, OUTPUT);
+  pinMode(A9G_PON, OUTPUT); // LOW LEVEL ACTIVE
+  // LOW LEVEL ACTIVE
 
-  digitalWrite(A9G_rstPin, LOW);
-  digitalWrite(A9G_rstPin, HIGH);
+  // digitalWrite(A9G_rstPin, LOW);
+  // digitalWrite(A9G_rstPin, HIGH);
+  delay(1000);
+  digitalWrite(A9G_PON, LOW);
+  delay(1000);
+  digitalWrite(A9G_PON, HIGH);
 
   String res = "";
   while (res.indexOf("READY") < 0)
@@ -39,23 +50,23 @@ void setup()
     Serial.println(res);
   };
   Serial.println("A9G resetted sucsessfully\n\r");
-
+  delay(1000);
   // check the status
-  sendCommand("AT", 3000, true);
+  sendCommand("AT", 2000, true);
   // //Attaching to the GPRS netwotk
-  sendCommand("AT+CGATT=1", 3000, true);
+  sendCommand("AT+CGATT=1", 2000, true);
   // //Setting the APN name
-  sendCommand("AT+CGDCONT=1,\"IP\",\"internet\"", 3000, true);
+  sendCommand("AT+CGDCONT=1,\"IP\",\"internet\"", 2000, true);
   // //Active command is used to active the specified PDPcontext
-  sendCommand("AT+CGACT=1,1", 3000, true);
+  sendCommand("AT+CGACT=1,1", 2000, true);
   // //check the status of the connection
-  sendCommand("AT+CIPSTATUS?", 3000, false);
+  sendCommand("AT+CIPSTATUS?", 2000, false);
   // //Turn on the GPS
-  sendCommand("AT+GPS=1", 3000, true);
+  sendCommand("AT+GPS=1", 2000, true);
   // //Activate low power mode for the gps
   sendCommand("AT+GPSLP=2", 2000, true);
   // //Checking the signal strength
-  sendCommand("AT+CSQ", 3000, false);
+  sendCommand("AT+CSQ", 2000, false);
   // Connect to Mqtt broker
   sendCommand("AT+MQTTCONN=\"" + broker + "\",1883,\"XXXX\",120,0", 3000, true);
   //  Send MQTT subscribe packet
@@ -66,12 +77,11 @@ void loop()
 {
 
   // Get and send GPS Coordinations
-  String location = sendCommand("AT+LOCATION=2", 2000, false, false);
+  String location = sendCommand("AT+LOCATION=2", 1000, false, false);
   String battery = sendCommand("AT+CBC?", 1000, true, false).substring(20, 22);
   getLocation(location, lat, lang);
-  sendCommand("AT+MQTTPUB=\"" + PublishTopic + "\",\"" + getPayload(presionerID, lat, lang, battery) + "\",0,0,0 ", 4000, false);
+  sendCommand("AT+MQTTPUB=\"" + PublishTopic + "\",\"" + getPayload(presionerID, lat, lang, battery) + "\",0,0,0 ", 2000, false);
 
-  Serial.println(getResponceFromBroker(6000));
   checkConnection();
   delay(1000);
 }
@@ -105,29 +115,31 @@ String getPayload(String presioner_ID, String latitude, String langitude, String
   return presioner_ID + separator + latitude + separator + langitude + separator + battery;
 }
 
-String getResponceFromBroker(int timeOut)
+void verifyBrokerResponce(String responce)
 {
-  long startTime = millis();
-  String responce = "";
-
-  while ((millis() - startTime) < timeOut)
+  if (responce.indexOf("alarm") >= 0)
   {
-    if (sfw.available())
-      responce += (char)sfw.read();
+    Serial.println("Test started: ");
+    for (short i = 0; i < 3; i++)
+    {
+      digitalWrite(buzzer_pin, HIGH);
+      delay(250);
+      digitalWrite(buzzer_pin, LOW);
+      delay(250);
+    }
+    Serial.println("Test Completed: ");
   }
-
-  return responce;
 }
-
 // fucntion to check if the module is still connected to the borker
 void checkConnection()
 {
   String responce;
 
-  responce = sendCommand("AT+MQTTCONN?", 2000, false, false);
+  responce = sendCommand("AT+MQTTCONN?", 1000, false, false);
 
   while (responce.indexOf("0") >= 0)
   {
+
     Serial.println("Connection lost , tring to connect again ....");
     sendCommand("AT+MQTTCONN=\"" + broker + "\",1883,\"XXXX\",120,0", 3000, true);
     sendCommand("AT+MQTTSUB=\"" + subTopic + "\",1,0", 3000, true);
@@ -154,6 +166,11 @@ String sendCommand(String command, int timeOut, bool checkForErrors, bool debug)
 
     if (debug)
       Serial.println("res is : " + responce);
-  } while (checkForErrors && (responce.indexOf("ERR") >= 0));
+    delay(1000);
+  } while (checkForErrors && (responce.indexOf("OK") < 0));
+  if (responce.indexOf("MQTTPUB") >= 0)
+  {
+    verifyBrokerResponce(responce);
+  }
   return responce;
 }
--- testServer/main.js
+++ testServer/main.js
@@ -2,10 +2,11 @@ const express = require("express");
 const mqtt = require("mqtt");
 const app = express();
 const broker = "mqtt://test.mosquitto.org:1883";
-const receiveTopic = "stuped11";
+const receiveTopic = "zmalaPublish";
 const client = mqtt.connect(broker); // Connect to a public broker for demo purposes
-const publishTopic = "stuppubsh";
+const publishTopic = "zamalSub";
 const listenPort = 3030;
+let startTime;
 console.clear();
 client.on("connect", function () {
   console.log("mqtt client connected");
@@ -19,13 +20,25 @@ app.post("/publish", (req, res) => {
 
 // Subscribe to a topic and log messages
 client.subscribe(receiveTopic, function (err) {
-  if (!err) console.log("Successfullly subscribed !!in topic " + receiveTopic);
-  else console.log("the error is " + err);
+  if (!err) {
+    console.log("Successfullly subscribed !!in topic " + receiveTopic);
+    startTime = new Date();
+  } else console.log("the error is " + err);
   console.log();
 });
 client.on("message", function (topic, message) {
   console.log("The Message is: " + message.toString());
-  client.publish(publishTopic, "Message Received , Alarm, good Battery");
+  const endTime = new Date();
+  const elapsedTime = endTime - startTime;
+  console.log("Time taken to receive the message: " + elapsedTime + "ms");
+  console.log(
+    "message recieved at : " +
+      `${endTime.getHours()}:${endTime.getMinutes()}:${endTime.getSeconds()}`
+  );
+  startTime = new Date();
+  // Your existing code here
+
+  client.publish(publishTopic, "alarm, good");
   console.log("-----------------------------------");
   console.log();
 });
